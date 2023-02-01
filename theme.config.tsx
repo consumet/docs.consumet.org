8388f0f4b3176d7862027880038673d98ccf15e3
Tdanks2000 <tommydanks2000@hotmail.com>
2023-02-01 22:03:01 +0000

    change patreon logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useTheme } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   gitTimestamp: false,
@@ -12,19 +12,26 @@ const config: DocsThemeConfig = {
   },
   logo: <span>Consumet Documentation</span>,
   navbar: {
-    extraContent: (
-      <a
-        href={`https://www.patreon.com/ConsumetOrg`}
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ width: "24px", height: "24px", margin: "0 .5rem" }}
-      >
-        <img
-          src="https://i0.wp.com/decentered.co.uk/wp-content/uploads/2019/12/patreon-logo-png-badge-7.png?fit=541%2C540&ssl=1"
-          alt="Become a Patron!"
-        />
-      </a>
-    ),
+    extraContent: () => {
+      const { theme } = useTheme();
+
+      return (
+        <a
+          href={`https://www.patreon.com/ConsumetOrg`}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ width: "24px", height: "24px", margin: "0 .5rem" }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            fill={theme === "dark" ? "#fff" : "#000"}
+          >
+            <path d="M512 194.8c0 101.3-82.4 183.8-183.8 183.8-101.7 0-184.4-82.4-184.4-183.8 0-101.6 82.7-184.3 184.4-184.3C429.6 10.5 512 93.2 512 194.8zM0 501.5h90v-491H0v491z" />
+          </svg>
+        </a>
+      );
+    },
   },
   project: {
     link: "https://github.com/consumet",
